4510c1a051bf61c2e58a7b764162e3f3f6b0bd21
Torgrim <sir_alexiner@hotmail.com>
2024-03-06 20:54:05 +0100

    Fix logging

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import json
 import tempfile
+import logging
+from logging.handlers import RotatingFileHandler
 import os
 import base64
 from flask import Flask, stream_template, request, jsonify, session, Response
+from flask.logging import default_handler
 from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA256
@@ -19,6 +22,26 @@ import handler.json_handler as jsonHand
 
 # Initialize Flask
 app = Flask(__name__)
+
+def configure_logging(app):
+    # Logging Configuration
+    if app.config['LOG_WITH_GUNICORN']:
+        gunicorn_error_logger = logging.getLogger('gunicorn.error')
+        app.logger.handlers.extend(gunicorn_error_logger.handlers)
+        app.logger.setLevel(logging.DEBUG)
+    else:
+        file_handler = RotatingFileHandler('instance/flask-user-management.log',
+                                           maxBytes=16384,
+                                           backupCount=20)
+        file_formatter = logging.Formatter('%(asctime)s %(levelname)s %(threadName)s-%(thread)d: %(message)s [in %(filename)s:%(lineno)d]')
+        file_handler.setFormatter(file_formatter)
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+    # Remove the default logger configured by Flask
+    app.logger.removeHandler(default_handler)
+
+    app.logger.info('...Starting the Flask Server...')
 
 # Set a secret key for session management
 app.secret_key = os.urandom(32)
